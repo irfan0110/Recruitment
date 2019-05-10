8ecda63433eb46d7a44b631d734ed9b2533bd451
fakewars <fakewarszero@gmail.com>
2019-05-10 15:49:47 +0700

    Tabbing

--- Recruitment/Controllers/CallController.cs
+++ Recruitment/Controllers/CallController.cs
@@ -11,53 +11,24 @@ namespace Recruitment.Controllers
     [RoutePrefix("call")]
     public class CallController : Controller
     {
-        List<CANDIDATE> candidates;
+        List<CandidateCallDTO> candidates;
+        //List<CandidateCallDTO> calledCandidates;
         // GET: Call
         [Route("")]
-        public ActionResult Index(string searchField, string searchString, int? page)
+        public ActionResult Index(string filterPosition, string searchString, int? page)
         {
             InitializeCandidates();
+            
             //SEARCHING
             if (!String.IsNullOrEmpty(searchString)) {
-                if(searchField == "name") {
-                    candidates = candidates.Where(c => c.NAMA_LENGKAP.Contains(searchString)).ToList();
-                }
-                else if (searchField == "appliedPosition") {
-                    candidates = candidates.Where(c => c.JUDUL_POSISI.Contains(searchString)).ToList();
-                }
-                else if (searchField == "source") {
-                    candidates = candidates.Where(c => c.SOURCE_ID.Contains(searchString)).ToList();
-                }
-                else if (searchField == "phoneNumber") {
-                    candidates = candidates.Where(c => c.NOHP.Contains(searchString)).ToList();
-                }
-                else if (searchField == "email") {
-                    candidates = candidates.Where(c => c.EMAIL.Contains(searchString)).ToList();
-                }
-                else if (searchField == "praSelectionPIC") {
-                    candidates = candidates.Where(c => c.USER_CREATE.Contains(searchString)).ToList();
-                }
-                else if (searchField == "praSelectionNotes") {
-                    candidates = candidates.Where(c => c.CATATAN.Contains(searchString)).ToList();
-                }
-
+                candidates = candidates.Where(c => c.Name.ToUpper().Contains(searchString.ToUpper())).ToList();
                 page = 1;
             }
 
-            //SORTING
-            string sortOrder = (string)TempData.Peek("SortOrder");
-
-            if(sortOrder == "id_desc") {
-                candidates = candidates.OrderByDescending(c => c.CANDIDATE_ID).ToList();
-            }
-            else if(sortOrder == "name_asc") {
-                candidates = candidates.OrderBy(c => c.NAMA_LENGKAP).ToList();
-            }
-            else if(sortOrder == "name_desc") {
-                candidates = candidates.OrderByDescending(c => c.NAMA_LENGKAP).ToList();
-            }
-            else { //Default sort is id_asc
-                candidates = candidates.OrderBy(c => c.CANDIDATE_ID).ToList();
+            if (!String.IsNullOrEmpty(filterPosition)) {
+                candidates = candidates.Where(c => c.Position.ToUpper().Equals(filterPosition.ToUpper())).ToList();
+                //TODO SEARCH ON ALL FIELDS
+                page = 1;
             }
 
             //PAGING
@@ -71,50 +42,142 @@ namespace Recruitment.Controllers
             return View("ListCall", candidates.ToPagedList(pageNumber,pageSize));
         }
 
-        [ActionName("SortByID")]
-        public ActionResult SortById() {
-            string sortOrder = (string)TempData.Peek("SortOrder");
-            if(sortOrder == "id_asc" || String.IsNullOrEmpty(sortOrder)) {
-                TempData["SortOrder"] = "id_desc";
-            }
-            else {
-                TempData["SortOrder"] = "id_asc";
+        [Route("called")]
+        public ActionResult IndexCalled(string filterPosition, string searchString, int? page) {
+            InitializeCalledCandidates();
+
+            //SEARCHING
+            if (!String.IsNullOrEmpty(searchString)) {
+                candidates = candidates.Where(c => c.Name.ToUpper().Contains(searchString.ToUpper())).ToList();
+                page = 1;
             }
 
-            return Redirect("~/call");
-        }
-
-        [ActionName("SortByName")]
-        public ActionResult SortByName() {
-            string sortOrder = (string)TempData.Peek("SortOrder");
-            if (sortOrder == "name_asc") {
-                TempData["SortOrder"] = "name_desc";
-            }
-            else {
-                TempData["SortOrder"] = "name_asc";
+            if (!String.IsNullOrEmpty(filterPosition)) {
+                candidates = candidates.Where(c => c.Position.ToUpper().Equals(filterPosition.ToUpper())).ToList();
+                //TODO SEARCH ON ALL FIELDS
+                page = 1;
             }
 
-            return Redirect("~/call");
+            //PAGING
+            //Require nuGet package PagedList.mvc
+            if (page == null) {
+                page = 1;
+            }
+            int pageSize = 5; //The amount of row displayed each page
+            int pageNumber = (page ?? 1);
+
+            return View("ListCalled", candidates.ToPagedList(pageNumber, pageSize));
         }
 
         void InitializeCandidates() {
             using(RecruitmentEntities db = new RecruitmentEntities()) {
-                candidates = db.CANDIDATEs.Where(c => c.STATE_ID == "ST001").ToList();
+               candidates = (from c in db.CANDIDATEs
+                              join s in db.SOURCEs on c.SOURCE_ID equals s.SOURCE_ID into table1
+                              from s in table1.DefaultIfEmpty()
+                                join u in db.USERs on c.USER_CREATE equals u.USERNAME into table2
+                                from u in table2.DefaultIfEmpty()
+                                    join st in db.STATEs on c.STATE_ID equals st.STATE_ID into table3
+                                    from st in table3.DefaultIfEmpty()
+                                    where c.STATE_ID == "ST001"
+                                    select new CandidateCallDTO { 
+                                        CandidateId = c.CANDIDATE_ID,
+                                        Name = c.NAMA_LENGKAP,
+                                        Position = c.JUDUL_POSISI,
+                                        Source = s.SOURCE_NAME,
+                                        Phone = c.NOHP,
+                                        Email = c.EMAIL,
+                                        PreSelectPIC = u.FULLNAME,
+                                        State = st.STATE_NAME,
+                                        Notes = c.CATATAN
+                                    }).ToList();
+
+                List<SelectListItem> filterPositions = db.POSITIONs.Select(p => new SelectListItem {
+                    Text = p.POSITION_NAME,
+                    Value = p.POSITION_NAME                
+                }).ToList();
+                filterPositions.Insert(0, (new SelectListItem { Text = "", Value = "" }));
+
+                TempData["filterPositions"] = filterPositions;
             }
         }
 
-        //void InitializeCandidateDummy() {
-        //    candidate = new List<CandidateDummy>() {
-        //        new CandidateDummy("CA_001", "Jon Champion","ST020"),
-        //        new CandidateDummy("CA_002", "Jqn Qhampion","ST020"),
-        //        new CandidateDummy("CA_003", "Jen Whampion","ST020"),
-        //        new CandidateDummy("CA_004", "Jrn Ehampion","ST020"),
-        //        new CandidateDummy("CA_005", "Jun Rhampion","ST020"),
-        //        new CandidateDummy("CA_006", "Jin Thampion","ST020"),
-        //        new CandidateDummy("CA_007", "Jhn Yhampion","ST020"),
-        //        new CandidateDummy("CA_008", "Jjn Uhampion","ST020"),
-        //        new CandidateDummy("CA_009", "Jkn Ihampion","ST020")
-        //    };
+        void InitializeCalledCandidates() {
+            using (RecruitmentEntities db = new RecruitmentEntities()) {
+                candidates = (from c in db.CANDIDATEs
+                              join s in db.SOURCEs on c.SOURCE_ID equals s.SOURCE_ID into table1
+                              from s in table1.DefaultIfEmpty()
+                              join u in db.USERs on c.USER_CREATE equals u.USERNAME into table2
+                              from u in table2.DefaultIfEmpty()
+                              join st in db.STATEs on c.STATE_ID equals st.STATE_ID into table3
+                              from st in table3.DefaultIfEmpty()
+                              where c.STATE_ID == "ST002"
+                              select new CandidateCallDTO {
+                                  CandidateId = c.CANDIDATE_ID,
+                                  Name = c.NAMA_LENGKAP,
+                                  Position = c.JUDUL_POSISI,
+                                  Source = s.SOURCE_NAME,
+                                  Phone = c.NOHP,
+                                  Email = c.EMAIL,
+                                  PreSelectPIC = u.FULLNAME,
+                                  State = st.STATE_NAME,
+                                  Notes = c.CATATAN
+                              }).ToList();
+
+                List<SelectListItem> filterPositions = db.POSITIONs.Select(p => new SelectListItem {
+                    Text = p.POSITION_NAME,
+                    Value = p.POSITION_NAME
+                }).ToList();
+                filterPositions.Insert(0, (new SelectListItem { Text = "", Value = "" }));
+
+                TempData["filterPositions"] = filterPositions;
+            }
+        }
+
+        #region UNUSED
+
+        //SORTING
+        //string sortOrder = (string)TempData.Peek("SortOrder");
+
+        //if(sortOrder == "id_desc") {
+        //    candidates = candidates.OrderByDescending(c => c.CANDIDATE_ID).ToList();
         //}
+        //else if(sortOrder == "name_asc") {
+        //    candidates = candidates.OrderBy(c => c.NAMA_LENGKAP).ToList();
+        //}
+        //else if(sortOrder == "name_desc") {
+        //    candidates = candidates.OrderByDescending(c => c.NAMA_LENGKAP).ToList();
+        //}
+        //else { //Default sort is id_asc
+        //    candidates = candidates.OrderBy(c => c.CANDIDATE_ID).ToList();
+        //}
+
+        //[ActionName("SortByID")]
+        //public ActionResult SortById() {
+        //    string sortOrder = (string)TempData.Peek("SortOrder");
+        //    if (sortOrder == "id_asc" || String.IsNullOrEmpty(sortOrder)) {
+        //        TempData["SortOrder"] = "id_desc";
+        //    }
+        //    else {
+        //        TempData["SortOrder"] = "id_asc";
+        //    }
+
+        //    return Redirect("~/call");
+        //}
+
+        //[ActionName("SortByName")]
+        //public ActionResult SortByName() {
+        //    string sortOrder = (string)TempData.Peek("SortOrder");
+        //    if (sortOrder == "name_asc") {
+        //        TempData["SortOrder"] = "name_desc";
+        //    }
+        //    else {
+        //        TempData["SortOrder"] = "name_asc";
+        //    }
+
+        //    return Redirect("~/call");
+        //}
+
+
+        #endregion
     }
 }
---
+++ Recruitment/Models/CandidateCallDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Recruitment.Models
+{
+    public class CandidateCallDTO
+    {
+        string candidateId;
+        string name;
+        string position;
+        string source;
+        string phone;
+        string email;
+        string preSelectPIC;        
+        string state;
+        string notes;
+
+        public string CandidateId { get => candidateId; set => candidateId = value; }
+        public string Name { get => name; set => name = value; }
+        public string Position { get => position; set => position = value; }
+        public string Source { get => source; set => source = value; }
+        public string Phone { get => phone; set => phone = value; }
+        public string Email { get => email; set => email = value; }
+        public string PreSelectPIC { get => preSelectPIC; set => preSelectPIC = value; }        
+        public string State { get => state; set => state = value; }
+        public string Notes { get => notes; set => notes = value; }        
+    }
+}
